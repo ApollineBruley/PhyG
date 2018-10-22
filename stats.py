bacd365d432e115ab5f36f8dcd8a4def7e1e24a3
Benedicte Colnet <benedicte.colnet@gmail.com>
2018-10-22 22:04:43 +0200

    Premier commit pour tester

--- stats.py
+++ stats.py
@@ -1,16 +1,24 @@
 import os,sys
+import numpy as np
 
 def resume_statistics(dir_results):
-	statistiques={} # Dictionnaire qui somme les occurences de chaque evenement, pour nous faciliter a fiare la moyenne
+	statistiques=np.zeros(10,7) # Tableau [evenement][type : Inversions, Translocations, Duplications, Deletions, Fusions, Fissions, WGD]
+	#On parcourt les fichiers results
+	numero_simu=0
 	for dir_res in os.listdir(dir_results):
 		fichier = dir_results+"/"+dir_res+"/statistics"
-		read_statistics(fichier,statistiques)
+		read_statistics(fichier,statistiques[numero_simu])
+		numero_simu+=1
 	return statistics
 
 
 def read_statistics(fichier_stat,dico_stat): # lit 1 fichier result
+	alllines = fichier_stat.readlines()
+	i=0
+	for line in alllines:
+        	if line[1:11] == 'Inversions':
+			dico_stat[i]=int(line[17:19])
 	return None # Retourne moyennes ou proportions
-
 
 
 def write_statistics(): # moyennes ou proportions
