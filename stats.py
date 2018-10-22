0342938cd44234efd7b87174044b8e3ace464ded
Apolline Bruley <apolline.bruley@hotmail.fr>
2018-10-22 10:09:44 +0200

    Creation script

---
+++ stats.py
@@ -0,0 +1,7 @@
+def read_statistics(): # fichiers ou repertoire results 
+	return None # Retourne moyennes ou proportions
+
+
+def write_statistics(): # moyennes ou proportions
+	# Les ecrit dans un fichier
+	return None
