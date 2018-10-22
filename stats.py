edadb58a4a78c6f138a5f779fb39171988638266
Benedicte Colnet <benedicte.colnet@gmail.com>
2018-10-22 22:56:46 +0200

    Fichier compelete et teste pour parseur

--- stats.py
+++ stats.py
@@ -2,28 +2,51 @@ import os,sys
 import numpy as np
 
 def resume_statistics(dir_results):
-	statistiques=np.zeros(10,7) # Tableau [evenement][type : Inversions, Translocations, Duplications, Deletions, Fusions, Fissions, WGD]
-	#On parcourt les fichiers results
-	numero_simu=0
-	for dir_res in os.listdir(dir_results):
-		fichier = dir_results+"/"+dir_res+"/statistics"
-		read_statistics(fichier,statistiques[numero_simu])
-		numero_simu+=1
-	return statistics
+    operation_type=['Inversions', 'Translocations', 'Duplications', 'Deletions', 'Fusions', 'Fissions', 'WGD']
+    compteur=np.zeros(10,7) # [simulation][Inversions, Translocations, Duplications, Deletions, Fusions, Fissions, WGD]
+    nb_simu=0	
+    for dir_res in os.listdir(dir_results):
+        	fichier = dir_results+"/"+dir_res+"/statistics"
+        	compteur=read_statistics(fichier,compteur, nb_simu)
+        	nb_simu=nb_simu+1
+    result= global_statistics(compteur)
+    total={}
+    for x in operation_type:
+     for y in result:
+        total[x] = y
+    print(total)
+    return 
 
 
-def read_statistics(fichier_stat,dico_stat): # lit 1 fichier result
-	alllines = fichier_stat.readlines()
-	i=0
-	for line in alllines:
-        	if line[1:11] == 'Inversions':
-			dico_stat[i]=int(line[17:19])
-	return None # Retourne moyennes ou proportions
+def read_statistics(fichier_stat, dico_stat, nb_simu): # lit 1 fichier result
+    alllines = fichier_stat.readlines()
+    for line in alllines:
+        print(line)
+        if line[0:10] == 'Inversions':
+            dico_stat[nb_simu][0]=int(line[16:19].strip())
+        elif line[0:14] =='Translocations':
+            dico_stat[nb_simu][1]=int(line[16:19].strip())
+        elif line[0:13] =='Duplications':
+            dico_stat[nb_simu][2]=int(line[16:19].strip())
+        elif line[0:9] =='Deletions':
+            dico_stat[nb_simu][3]=int(line[16:19].strip())
+        elif line[0:7] =='Fusions':
+            dico_stat[nb_simu][4]=int(line[16:19].strip())
+        elif line[0:8] =='Fissions':
+            dico_stat[nb_simu][5]=int(line[16:19].strip())
+        elif line[0:8] =='WGD':
+            dico_stat[nb_simu][6]=int(line[16:19].strip())
+    return dico_stat
 
 
-def write_statistics(): # moyennes ou proportions
-	# Les ecrit dans un fichier
-	return None
+
+def global_statistics(res): # moyennes ou proportions
+    l=np.zeros(7)
+    size=res.shape
+    for i in range(len(size[0])):
+        for j in range(len(size[1])):
+            l[j]=l[j]+res[i][j]
+    return l
 
 try:
 	dir_results = sys.argv[1]
