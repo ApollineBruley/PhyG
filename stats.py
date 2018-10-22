f1a1984f94fbef32b38132b82492e573eeb229b8
Apolline Bruley <apolline.bruley@hotmail.fr>
2018-10-22 19:03:11 +0200

    repertoire Results donne en argument au lancement du script

--- stats.py
+++ stats.py
@@ -1,4 +1,4 @@
-import os
+import os,sys
 
 def resume_statistics(dir_results):
 	statistiques={} # Dictionnaire qui somme les occurences de chaque evenement, pour nous faciliter a fiare la moyenne
@@ -10,9 +10,15 @@ def resume_statistics(dir_results):
 
 def read_statistics(fichier_stat,dico_stat): # lit 1 fichier result
 	return None # Retourne moyennes ou proportions
-	
+
 
 
 def write_statistics(): # moyennes ou proportions
 	# Les ecrit dans un fichier
 	return None
+
+try:
+	dir_results = sys.argv[1]
+	statistics = resume_statistics(dir_results)
+except:
+	print("Veuillez entrer le nom du repertoire contenant les fichiers statistics en argument.")
