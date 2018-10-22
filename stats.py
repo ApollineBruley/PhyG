2d16b422f215c56ce7663704ef2421e501d5c670
Apolline Bruley <apolline.bruley@hotmail.fr>
2018-10-22 19:00:01 +0200

    parcours de tous les repertoires de Result pour trouver les fichiers statistics

--- stats.py
+++ stats.py
@@ -1,5 +1,16 @@
-def read_statistics(): # fichiers ou repertoire results 
+import os
+
+def resume_statistics(dir_results):
+	statistiques={} # Dictionnaire qui somme les occurences de chaque evenement, pour nous faciliter a fiare la moyenne
+	for dir_res in os.listdir(dir_results):
+		fichier = dir_results+"/"+dir_res+"/statistics"
+		read_statistics(fichier,statistiques)
+	return statistics
+
+
+def read_statistics(fichier_stat,dico_stat): # lit 1 fichier result
 	return None # Retourne moyennes ou proportions
+	
 
 
 def write_statistics(): # moyennes ou proportions
